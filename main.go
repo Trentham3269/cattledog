bab181ea7af4846ad854ad0e8e380f03bc9efcac
Trentham3269 <scottdalwood@gmail.com>
2019-05-12 08:04:34 +0000

    Connect to managed postgres database

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	//"fmt"
+	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
+	//"strconv"
 
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
 	"github.com/gorilla/mux"
 )
 
@@ -17,37 +21,86 @@ type Category struct {
 
 var categories []Category
 
-func main() {
-	router := mux.NewRouter()
+var db *sql.DB
 
-	categories = append(categories,
-		Category{ID: 1, Name: "Soccer"},
-		Category{ID: 2, Name: "Basketball"},
-		Category{ID: 3, Name: "Baseball"},
-		Category{ID: 4, Name: "Frisbee"},
-		Category{ID: 5, Name: "Snowboarding"},
-		Category{ID: 6, Name: "Rock Climbing"},
-		Category{ID: 7, Name: "Football"},
-		Category{ID: 8, Name: "Surfing"},
-		Category{ID: 9, Name: "Hockey"})
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
+
+	db, err = sql.Open("postgres", pgUrl)
+	logFatal(err)
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("Could not connect to database")
+	} else {
+		log.Println("Connected to database successfully")
+	}
+	defer db.Close()
+
+	router := mux.NewRouter()
 
 	router.HandleFunc("/categories", getCategories).Methods("GET")
 	router.HandleFunc("/categories/{id}", getCategory).Methods("GET")
+	router.HandleFunc("/categories", addCategory).Methods("POST")
+	router.HandleFunc("/categories", updateCategory).Methods("PUT")
+	router.HandleFunc("/categories/{id}", deleteCategory).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8888", router))
+	port := 8888
+	host := fmt.Sprintf("localhost:%d", port)
+	log.Println(fmt.Sprintf("Server listening on %d...", port))
+	log.Fatal(http.ListenAndServe(host, router))
 }
 
 func getCategories(w http.ResponseWriter, r *http.Request) {
+	var category Category
+	categories = []Category{}
+
+	rows, err := db.Query("select * from categories")
+	logFatal(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&category.ID, &category.Name)
+		logFatal(err)
+
+		categories = append(categories, category)
+	}
+
 	json.NewEncoder(w).Encode(categories)
 }
 
 func getCategory(w http.ResponseWriter, r *http.Request) {
+	var category Category
 	params := mux.Vars(r)
-	i, _ := strconv.Atoi(params["id"])
 
-	for _, category := range categories {
-		if category.ID == i {
-			json.NewEncoder(w).Encode(&category)
-		}
-	}
+	row := db.QueryRow("select * from categories where id=$1", params["id"])
+
+	err := row.Scan(&category.ID, &category.Name)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(category)
+}
+
+
+func addCategory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateCategory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteCategory(w http.ResponseWriter, r *http.Request) {
+
 }
